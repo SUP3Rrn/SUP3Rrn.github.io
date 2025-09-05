c1b62a3e5202f414979b5eec947321d748a00e12
SUP3R 3 <danielnour.1006@gmail.com>
2025-09-05 12:07:29 +0000

    python

--- provapython.py
+++ provapython.py
@@ -1,1 +1,13 @@
-print ("ciao")
+testo = input("Scrivi qualcosa: ")
+if testo == "ciao":
+    print("Ciao anche a te!")
+
+if testo == "qualcosa":
+    print("haha che simpatico")
+    
+if testo == "sei scemo":
+    print("mi offendo ora eh")
+
+operazione = input("Scrivi un'operazione (es: 2*5): ")
+risultato = eval(operazione)
+print("Risultato:", risultato)
