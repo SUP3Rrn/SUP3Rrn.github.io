731775839ab31d346a4bf8c5d66b06922166f85f
SUP3R 3 <danielnour.1006@gmail.com>
2025-11-28 23:35:26 +0000

    Finito C

--- prova del c.c
+++ prova del c.c
@@ -1,3 +1,31 @@
 int main() {
-    printf(ciao amico!)
+printf("Ora ti farò una domanda! Sei pronto/a?\n");
+
+ char nome[50];
+ char risposta[4];
+                    
+ printf("Qual è il tuo nome?\n");
+                            
+ scanf("%s", nome);
+                                    
+printf("Ciao, %s!\n", nome);
+
+printf("Come stai?\n");
+
+scanf("%s", risposta);
+
+if (strcmp(risposta, "bene") == 0) {
+// Se la risposta è "bene"
+printf("Ah, mi fa piacere che stai bene!\n");
+}
+else if (strcmp(risposta, "male") == 0) {
+
+printf("Ah, mi dispiace che stai male :( \n");
+}
+else {
+// Se la risposta è diversa da "bene" o "male"
+printf("Non ho capito, ma spero che tu stia bene!\n");
+}
+
+return 0;
 }
